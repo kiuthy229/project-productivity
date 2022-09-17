f523d44434f6e2c5c7b02348c43da8a6efeb0e15
kieuthy <thynmkgcs190847@fpt.edu.vn>
2022-09-17 21:49:35 +0700

    chat with people

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import CreateRoom from "./routes/CreateRoom";
 import SingleCamera from "./routes/SingleCamera";
 import ToDoList from './routes/ToDoList';
+import JoinRoom from './routes/JoinRoom'
 import Room from "./routes/Room";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={CreateRoom} />
+        <Route path="/join" exact component={JoinRoom} />
         {/* <Route path="/" exact component={SingleCamera} /> */}
         <Route path="/todolist" exact component={ToDoList} />
         <Route path="/room/:roomID" component={Room} />
